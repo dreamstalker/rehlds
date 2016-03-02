a43c452428fb3a35aec82f52e091da83004bf7b9
asmodai <asmodai@dedicated-server.ru>
2016-03-02 20:20:14 +0300

    Added Cvar_DirectSet hookchain

--- rehlds/public/rehlds/rehlds_api.h
+++ rehlds/public/rehlds/rehlds_api.h
@@ -35,7 +35,7 @@
 #include "model.h"
 
 #define REHLDS_API_VERSION_MAJOR 2
-#define REHLDS_API_VERSION_MINOR 8
+#define REHLDS_API_VERSION_MINOR 9
 
 //Steam_NotifyClientConnect hook
 typedef IHookChain<qboolean, IGameClient*, const void*, unsigned int> IRehldsHook_Steam_NotifyClientConnect;
@@ -169,6 +169,10 @@ typedef IHookChainRegistry<int> IRehldsHookRegistry_SV_TransferConsistencyInfo;
 typedef IHookChain<bool, uint64, const char *, uint32> IRehldsHook_Steam_GSBUpdateUserData;
 typedef IHookChainRegistry<bool, uint64, const char *, uint32> IRehldsHookRegistry_Steam_GSBUpdateUserData;
 
+//Cvar_DirectSet hook
+typedef IVoidHookChain<struct cvar_s *, const char *> IRehldsHook_Cvar_DirectSet;
+typedef IVoidHookChainRegistry<struct cvar_s *, const char *> IRehldsHookRegistry_Cvar_DirectSet;
+
 class IRehldsHookchains {
 public:
 	virtual ~IRehldsHookchains() { }
@@ -206,6 +210,7 @@ public:
 	virtual IRehldsHookRegistry_Steam_GSGetSteamID* Steam_GSGetSteamID() = 0;
 	virtual IRehldsHookRegistry_SV_TransferConsistencyInfo* SV_TransferConsistencyInfo() = 0;
 	virtual IRehldsHookRegistry_Steam_GSBUpdateUserData* Steam_GSBUpdateUserData() = 0;
+	virtual IRehldsHookRegistry_Cvar_DirectSet* Cvar_DirectSet() = 0;
 };
 
 struct RehldsFuncs_t {
@@ -251,7 +256,7 @@ struct RehldsFuncs_t {
 	void*(*GetPluginApi)(const char *name);
 	void(*RegisterPluginApi)(const char *name, void *impl);
 	qboolean(*SV_FileInConsistencyList)(const char *filename, struct consistency_s **ppconsist);
-	qboolean(*Steam_NotifyClientConnect_api)(IGameClient *cl, const void *pvSteam2Key, unsigned int ucbSteam2Key);
+	qboolean(*Steam_NotifyClientConnect)(IGameClient *cl, const void *pvSteam2Key, unsigned int ucbSteam2Key);
 	void(*Steam_NotifyClientDisconnect)(IGameClient* cl);
 };
 
