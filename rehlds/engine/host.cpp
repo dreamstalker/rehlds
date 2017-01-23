105075e0f682b7a099d6672971e9b77fca8cd2ff
In-line <cplusplus256@gmail.com>
2017-01-23 13:16:03 +0400

    Micro optimization and report recursive shutdown by Sys_Error (#318)
    * Micro optimization in CheckGore

--- rehlds/engine/host.cpp
+++ rehlds/engine/host.cpp
@@ -1008,20 +1008,12 @@ int Host_Frame(float time, int iState, int *stateInfo)
 
 void CheckGore(void)
 {
-	if (bLowViolenceBuild)
-	{
-		Cvar_SetValue("violence_hblood", 0.0);
-		Cvar_SetValue("violence_hgibs", 0.0);
-		Cvar_SetValue("violence_ablood", 0.0);
-		Cvar_SetValue("violence_agibs", 0.0);
-	}
-	else
-	{
-		Cvar_SetValue("violence_hblood", 1.0);
-		Cvar_SetValue("violence_hgibs", 1.0);
-		Cvar_SetValue("violence_ablood", 1.0);
-		Cvar_SetValue("violence_agibs", 1.0);
-	}
+	float fValue = bLowViolenceBuild ? 0.0f : 1.0f;
+	
+	Cvar_SetValue("violence_hblood", fValue);
+	Cvar_SetValue("violence_hgibs", fValue);
+	Cvar_SetValue("violence_ablood", fValue);
+	Cvar_SetValue("violence_agibs", fValue);
 }
 
 qboolean Host_IsSinglePlayerGame(void)
