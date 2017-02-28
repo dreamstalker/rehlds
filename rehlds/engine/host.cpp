f1a27952429f28e25362755235631c615257025e
In-line <cplusplus256@gmail.com>
2017-02-28 19:03:29 +0400

    Update host.cpp

--- rehlds/engine/host.cpp
+++ rehlds/engine/host.cpp
@@ -710,7 +710,11 @@ qboolean Host_FilterTime(float time)
 			if (fps > 100.0f)
 				fps = 100.0f;
 		}
+#ifdef REHLDS_FIXES
 		if (!Host_IsSinglePlayerGame())
+#else
+		if(g_pcl.maxclients > 1)
+#endif
 		{
 			if (fps < 20.0f)
 				fps = 20.0f;
