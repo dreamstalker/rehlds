5f1672ca85252d74fb501f582f6e1477700f8a9f
s1lent <s1lentsk@yandex.ru>
2017-12-09 16:47:08 +0700

    Move math sse to macros REHLDS_SSE

--- rehlds/engine/mathlib_e.h
+++ rehlds/engine/mathlib_e.h
@@ -30,8 +30,9 @@
 
 #include "maintypes.h"
 #include "model.h"
+#include "mathlib_sse.h"
 
-#ifndef REHLDS_FIXES
+#if !defined(REHLDS_FIXES) && !defined(REHLDS_SSE)
 // NOTE: In some cases we need high precision of floating-point,
 // so use double instead of float, otherwise unittest will fail
 typedef double real_t;
@@ -76,59 +77,6 @@ extern vec3_t vec3_origin;
 static const int nanmask = 0x7F800000;
 
 #define IS_NAN(fvar) ((*reinterpret_cast<int*>(&(fvar)) & nanmask) == nanmask)
-
-inline float M_sqrt(float value) {
-	return _mm_cvtss_f32(_mm_sqrt_ss(_mm_load_ss(&value)));
-}
-
-inline double M_sqrt(double value) {
-	auto v = _mm_load_sd(&value);
-	return _mm_cvtsd_f64(_mm_sqrt_sd(v, v));
-}
-
-template<typename T>
-inline double M_sqrt(T value) {
-	return sqrt(value);
-}
-
-inline float M_min(float a, float b) {
-	return _mm_cvtss_f32(_mm_min_ss(_mm_load_ss(&a), _mm_load_ss(&b)));
-}
-
-inline double M_min(double a, double b) {
-	return _mm_cvtsd_f64(_mm_min_sd(_mm_load_sd(&a), _mm_load_sd(&b)));
-}
-
-template<typename T>
-inline T M_min(T a, T b) {
-	return min(a, b);
-}
-
-inline float M_max(float a, float b) {
-	return _mm_cvtss_f32(_mm_max_ss(_mm_load_ss(&a), _mm_load_ss(&b)));
-}
-
-inline double M_max(double a, double b) {
-	return _mm_cvtsd_f64(_mm_max_sd(_mm_load_sd(&a), _mm_load_sd(&b)));
-}
-
-template<typename T>
-inline T M_max(T a, T b) {
-	return max(a, b);
-}
-
-inline float M_clamp(float a, float min, float max) {
-	return _mm_cvtss_f32(_mm_min_ss(_mm_max_ss(_mm_load_ss(&a), _mm_load_ss(&min)), _mm_load_ss(&max)));
-}
-
-inline double M_clamp(double a, double min, double max) {
-	return _mm_cvtsd_f64(_mm_min_sd(_mm_max_sd(_mm_load_sd(&a), _mm_load_sd(&min)), _mm_load_sd(&max)));
-}
-
-template<typename T>
-inline T M_clamp(T a, T min, T max) {
-	return clamp(a, min, max);
-}
 
 inline void VectorAdd(const vec_t *veca, const vec_t *vecb, vec_t *out)
 {
@@ -208,7 +156,7 @@ NOBODY void AngleIMatrix(const vec_t *angles, float *matrix);
 NOBODY void NormalizeAngles(float *angles);
 NOBODY void InterpolateAngles(float *start, float *end, float *output, float frac);
 void VectorTransform(const vec_t *in1, float (*in2)[4], vec_t *out);
-int VectorCompare(const vec_t *v1, const vec_t *v2);
+qboolean VectorCompare(const vec_t *v1, const vec_t *v2);
 void VectorMA(const vec_t *veca, float scale, const vec_t *vecm, vec_t *out);
 real_t _DotProduct(const vec_t *v1, const vec_t *v2);
 NOBODY void _VectorSubtract(vec_t *veca, vec_t *vecb, vec_t *out);
@@ -219,7 +167,6 @@ float Length(const vec_t *v);
 float Length2D(const vec_t *v);
 float VectorNormalize(vec_t *v);
 NOBODY void VectorInverse(vec_t *v);
-void VectorScale(const vec_t *in, float scale, vec_t *out);
 NOBODY int Q_log2(int val);
 NOBODY void VectorMatrix(vec_t *forward, vec_t *right, vec_t *up);
 void VectorAngles(const vec_t *forward, vec_t *angles);
