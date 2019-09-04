93c7709eea8e35a0ba550d1bb9b5038be105a954
Dominik Szamburski <dominikszamburski99@gmail.com>
2019-09-04 20:32:10 +0200

    use _vsnprintf_s on windows

--- rehlds/public/tier0/dbg.h
+++ rehlds/public/tier0/dbg.h
@@ -422,7 +422,11 @@ public:
 		va_list arg_ptr;
 
 		va_start(arg_ptr, pszFormat);
+#ifdef _WIN32
+		_vsnprintf_s(m_szBuf, sizeof(m_szBuf) - 1, sizeof(m_szBuf) - 1, pszFormat, arg_ptr);
+#else
 		_vsnprintf(m_szBuf, sizeof(m_szBuf) - 1, pszFormat, arg_ptr);
+#endif
 		va_end(arg_ptr);
 
 		m_szBuf[sizeof(m_szBuf) - 1] = 0;
