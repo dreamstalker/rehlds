462484a644c0de43bd74fd030ca5c15fde00892c
Karol Szuster <karolsz9898@gmail.com>
2020-09-07 17:19:29 +0200

    Remove unnecessary qualifier (#781)
    Signed-off-by: Karol Szuster <karolsz9898@gmail.com>

--- rehlds/dlls/player.h
+++ rehlds/dlls/player.h
@@ -304,7 +304,7 @@ public:
 	void SetCustomDecalFrames( int nFrames );
 	int GetCustomDecalFrames( void );
 
-	void CBasePlayer::TabulateAmmo( void );
+	void TabulateAmmo( void );
 
 	float m_flStartCharge;
 	float m_flAmmoStartCharge;
