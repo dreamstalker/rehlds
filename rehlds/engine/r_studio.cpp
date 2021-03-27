3c63e415332aec071a33abda8fb27c231af7d9f5
s1lentq <s1lentsk@yandex.ru>
2021-03-27 22:50:57 +0700

    QuaternionSlerp: Fix wrong values Related #763

--- rehlds/engine/r_studio.cpp
+++ rehlds/engine/r_studio.cpp
@@ -187,9 +187,9 @@ void QuaternionSlerp(vec_t *p, vec_t *q, float t, vec_t *qt)
 
 	cosom = p[0] * q[0] + p[1] * q[1] + p[2] * q[2] + p[3] * q[3];
 
-	if ((1.0 + cosom) > 0.00000001)
+	if ((1.0 + cosom) > 0.000001)
 	{
-		if ((1.0 - cosom) > 0.00000001)
+		if ((1.0 - cosom) > 0.000001)
 		{
 			omega = acos(cosom);
 			sinom = sin(omega);
