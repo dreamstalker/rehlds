46130dd3475fcf06fc8e05536f4117da7dc8ab1a
ShadowsAdi <54354938+ShadowsAdi@users.noreply.github.com>
2022-03-04 20:24:24 +0200

    Update pr_cmds.cpp

--- rehlds/engine/pr_cmds.cpp
+++ rehlds/engine/pr_cmds.cpp
@@ -1389,6 +1389,7 @@ int EXT_FUNC PF_precache_model_I(const char *s)
 int EXT_FUNC PF_precache_model_I_internal(const char *s)
 {
 	int iOptional = 0;
+	Con_Printf("Model: %s", char);
 	if (!s)
 		Host_Error("%s: NULL pointer", __func__);
 
